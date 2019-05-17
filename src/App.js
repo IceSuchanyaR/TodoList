c34e0a7842963a18bf2399afa2f48ab8831be68d
DESKTOP-9MGD9U7\MAY <suchanya.roekplain@gmail.com>
2019-05-17 18:59:27 +0700

    firstTest

--- src/App.js
+++ src/App.js
@@ -1,93 +1,64 @@
 import React, { Component } from 'react';
-import Header from './Header.js';
-import CompletedSection from './CompletedSection.js';
 import './App.css';
-import { List } from './List';
-
 class App extends Component {
-  state = {
-    list: [{
-      title: 'Task 1',
-      isCompleted: true,
-    }, {
-      title: 'Task 2',
-      isCompleted: false,
-    }, {
-      title: 'Task 3',
-      isCompleted: false,
-    }, {
-      title: 'Task 4',
-      isCompleted: true,
-    }],
-    showCompleted: false,
-  };
-
-  handleChangeToggle = (event) => {
-    const { checked, id } = event.target
-
-    this.state.list[id].isCompleted = checked
-    this.setState({
-      list: [
-        ...this.state.list
-      ]
-    })
-  }
-
-  handleChangeTitle = (event) => {
-    const { value, id } = event.target
-    this.state.list[id].title = value
-    this.setState({
-      list: [
-        ...this.state.list
-      ]
-    })
-  }
-
-  handleAdd = () => {
-    this.setState({
-      list: [
-        ...this.state.list,
-        { title: "", isCompleted: false }
-      ]
-    })
-  }
-
-  handleDelete = (event) => {
-    const { id } = event.target // get id
-    this.state.list.splice(id, 1) // delete
-    this.setState({
-      list: [
-        ...this.state.list
-      ]
-    })
-  }
-
   render() {
-    const completed = [];
-    const uncompleted = [];
-
-    this.state.list.filter((item,index) => {
-      if (item.isCompleted === true) {
-        completed.push({...item,index});
-      } else {
-        uncompleted.push({...item,index});
-      }
-    })
     return (
-      <div className="App col-lg-6">
-        <Header handleAdd = {this.handleAdd} />
-        <CompletedSection  list={completed}
-        handleChangeToggle={this.handleChangeToggle}
-        handleChangeTitle={this.handleChangeTitle}
-        handleDelete={this.handleDelete} />
-      <List
-       list={uncompleted}
-       handleChangeToggle = {this.handleChangeToggle}
-       handleChangeTitle={this.handleChangeTitle}
-       handleDelete={this.handleDelete} />
-      </div>
-    )
+      <div className="App">
+        <header className="w3-container w3-theme w3-padding" id="myHeader">
+
+          <div class="w3-row w3-border">
+            <div class="w3-half w3-container ">   
+            </div>
+            <div class="w3-half w3-container">
+              <div className="w3-center">
+                <h1>SAMUEL ANDERSON</h1>
+                <h2>THE EXPERIENCE UI/UX</h2>
+                <div className="w3-padding-32">
+
+                </div>
+                <div class="w3-row w3-xlarge">
+                  <div class="w3-col w3-container m2 w3-teal"><p>HOME</p></div>
+                  <div class="w3-col w3-container m2 w3-blue"><p>RESUME</p></div>
+                  <div class="w3-col w3-container m2 w3-purple"><p>PORTFOLIO</p></div>
+                  <div class="w3-col w3-container m2 w3-orange"><p>CONTATCS</p></div>
+                  <div class="w3-col w3-container m2 w3-red"><p>FEEDBACK</p></div>
+                  <div class="w3-col w3-container m2 w3-yellow"><p>BLOG</p></div>
+                  </div>
+                  </div>
+                  </div>
+            </div>
+        </header>
+          <div className="app-test">
+            <div class="w3-row w3-border">
+              <div class="w3-quarter w3-container w3-center">
+              <h1>MMMMMM</h1>  
+                <h5>w3-quarter</h5>
+                <p>The w3-quarter class uses one quarter (25%) of the screen window.</p>
+                <p>On small screens (max 600 pixels) it automatically resizes to full screen width.</p>
+              </div>
+              <div class="w3-quarter w3-container w3-center">
+              <h1>SIMPLE CV</h1>       
+              <p>On small screens (max 600 pixels) it automatically resizes to full screen width.</p>       
+              </div>
+              <div class="w3-quarter w3-container w3-center">
+              <h1>RESPONSIVE</h1>  
+              <span class="float-right badge badge-danger badge-pill" >SAVE</span>
+              <p>On small  resizes to full screen width.</p>      
+              </div>
+              <div class="w3-quarter w3-container w3-grey">
+                <h1>PERSONAL INFO</h1>
+                <p>NAME  : Ice Suchanya </p>
+                <p>BIRTH DATE   : 10/10/39</p>
+                <p>ADDRESS      : 166/16 , Chalongkrung Road ,LadKrabang,Bangkok  10520</p>
+                <p>PHONE :  096-7104026 </p>
+                <p>EMAILL : suchanya.ro@gmail.com</p>
+               
+              </div>
+            </div>
+          </div>        
+     </div>
+
+        );
   }
 }
-
 export default App;
